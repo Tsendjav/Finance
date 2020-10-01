d4867a7235322891e1c1064b5e28a66f541c898c
tsendee <tsendjav@outlook.com>
2020-10-01 14:57:14 +0800

    css iig https eer duudagddag bolgoh

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ var appController = (function (uiController, financeController) {
   });
 
   document.addEventListener("keypress", function (event) {
-    if (event.keyCode === 13 || event.which === 13) ctrlAddItem();
+    if (event.keyCode === 13 || event.which === 13) {
+      ctrlAddItem();
+    }
   });
 })(uiController, financeController);
