645a07ec480afa469e4d09ab9ec7a113024a5927
tsendee <tsendjav@outlook.com>
2020-10-02 11:42:01 +0800

    Orlogo zarlagiig delgets deer haruula

--- app.js
+++ app.js
@@ -1,36 +1,53 @@
 // Дэлгэцтэй ажиллах контроллер
 var uiController = (function () {
-
   var DOMstrings = {
-    inputType: '.add__type',
-    inputDescription: '.add__description',
-    inputValue: '.add__value',
-    addBtn: ".add__btn"
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
   };
-
   return {
     getInput: function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
         description: document.querySelector(DOMstrings.inputDescription).value,
-        value: document.querySelector(DOMstrings.inputValue).value
+        value: document.querySelector(DOMstrings.inputValue).value,
       };
     },
 
     getDOMstrings: function () {
       return DOMstrings;
-    }
+    },
+
+    addListItem: function (item, type) {
+      // Орлого зарлагын элементийг агуулсан html-ийг бэлтгэнэ.
+      var html;
+      if (type === "inc") {
+        list = ".income__list";
+        html =
+          '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else list = ".expenses__list";
+      html =
+        '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">$$VALUE$$</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div></div>';
+
+      // Тэр html дотроо орлого зарлагын утгуудыг REPLACE ашиглан өөрчилж өгнө.
+      html = html.replace("%id%", item.id);
+      html = html.replace("%description%", item.description);
+      html = html.replace("$$VALUE$$", item.value);
+
+      // Бэлтгэсэн html-ээ DOM руу хийж өгнө.
+      document.querySelector(list).insertAdjacentHTML("beforeend", html);
+    },
   };
 })();
 
 //Санхүүтэй ажиллах контроллер
 var financeController = (function () {
-
   var Income = function (id, description, value) {
     this.id = id;
     this.description = description;
     this.value = value;
-  }
+  };
 
   var Expense = function (id, description, value) {
     this.id = id;
@@ -47,13 +64,13 @@ var financeController = (function () {
   var data = {
     items: {
       inc: [],
-      exp: []
+      exp: [],
     },
 
     totals: {
       inc: 0,
-      exp: 0
-    }
+      exp: 0,
+    },
   };
   return {
     addItem: function (type, desc, val) {
@@ -64,42 +81,42 @@ var financeController = (function () {
         id = data.items[type][data.items[type].length - 1].id + 1;
       }
 
-      if (type === 'inc') {
+      if (type === "inc") {
         item = new Income(id, desc, val);
       } else {
         item = new Expense(id, desc, val);
       }
 
       data.items[type].push(item);
+
+      return item;
     },
     seeData: function () {
       return data;
-    }
-
-  }
-
-
-
+    },
+  };
 })();
 
 // Программын холбогч контроллер
 var appController = (function (uiController, financeController) {
-
   var ctrlAddItem = function () {
     // 1. Оруулах өгөгдлийг дэлгэцнээс олж авна.
     var input = uiController.getInput();
-    console.log(input);
 
     // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
-    financeController.addItem(input.type, input.description, input.value);
+    var item = financeController.addItem(
+      input.type,
+      input.description,
+      input.value
+    );
 
     // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана.
+    uiController.addListItem(item, input.type);
     // 4. Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
   };
 
   var setupEventListeners = function () {
-
     var DOM = uiController.getDOMstrings();
 
     document.querySelector(DOM.addBtn).addEventListener("click", function () {
@@ -114,11 +131,10 @@ var appController = (function (uiController, financeController) {
   };
   return {
     init: function () {
-      console.log('Application started...')
+      console.log("Application started...");
       setupEventListeners();
-    }
+    },
   };
-
 })(uiController, financeController);
 
-appController.init(); 
+appController.init();
